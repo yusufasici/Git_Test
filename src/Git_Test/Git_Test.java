532d49525917b37b537ff614969b28afd1f71a9d
yusufasici <68306103+yusufasici@users.noreply.github.com>
2020-07-14 23:48:27 -0400

    Update Git_Test.java

--- src/Git_Test/Git_Test.java
+++ src/Git_Test/Git_Test.java
@@ -3,5 +3,6 @@ package Git_Test;
 public class Git_Test {
     public static void main(String[] args) {
         System.out.println("Git Testi Yapiliyor");
+        System.out.println("Github Uzerinden Degisiklik Yapiliyor");
     }
 }
