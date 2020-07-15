5dc6d195d9c7befe03755d33d609ef639bacd468
yusufasici <yusuf.asici@gmail.com>
2020-07-14 23:42:39 -0400

    Ikinci Versiyon

--- src/Git_Test/Git_Test.java
+++ src/Git_Test/Git_Test.java
@@ -2,6 +2,6 @@ package Git_Test;
 
 public class Git_Test {
     public static void main(String[] args) {
-        System.out.println("Git Testi");
+        System.out.println("Git Testi Yapiliyor");
     }
 }
